5a7ead332186a6430516aa1f9b0cc390a5550d3b
Keenan04 <keenan.nasrallah@ocsbstudent.ca>
2021-05-20 10:35:12 -0400

    Create Rectangle.java

---
+++ src/shapes/Rectangle.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package shapes;
+
+/**
+ *
+ * @author keena
+ */
+class Rectangle {
+    private double length;
+    private double width;
+    private double area;
+    private double perimeter;
+
+public Rectangle(){
+this.setLength(1);
+this.setWidth(1);
+}
+
+public Rectangle (double side){
+   this.setLength(side);
+   this.setWidth(side);
+}
+
+public Rectangle(double length, double width){
+this.setLength(length);
+this.setWidth(width);
+}
+
+public double getLength() {
+    return length;
+}
+
+public void setLength (double length) {
+    this.length =length;
+    this.setArea();
+    this.setPerimeter();
+}
+
+public double getWidth() {
+    return width;
+}
+
+public void setWidth (double width) {
+    this.width=width;
+    this.setArea();
+    this.setPerimeter();
+}
+
+private void setArea(){
+    this.area=this.width*this.length;
+}
+    
+private void setPerimeter(){
+    this.perimeter = this.width*2+ this.length*2;
+    
+}
+
+    public double getArea() {
+        return area;
+    }
+
+    public double getPerimeter() {
+        return perimeter;
+    }
+      
+    
+}
